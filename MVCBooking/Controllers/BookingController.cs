1d1fb568337d355dc4d433292790e88e3af57743
Mihael Krpes <mikrpes@vsite.hr>
2023-11-02 16:32:20 +0100

    Add project files.

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        public IActionResult Index(){
+            return View(bookings);
+        }
+
+        public IActionResult Create(){
+            HotelBooking booking= new HotelBooking();
+            return View(booking);
+        }
+
+        public IActionResult CreateBooking(HotelBooking hotelBookingViewModel) {
+            bookings.Add(hotelBookingViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCBooking/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCBooking.Models
+{
+    public class HotelBooking
+    {
+        public int ID { get; set; }
+        public string name {  get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
